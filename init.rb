2a34c59030ff4ee5cca0bc8f2498cce488eedda7
onozaty <onozaty@gmail.com>
2014-02-11 14:13:18 +0000

    - Added parent issue filter. - Update plugin information.

--- init.rb
+++ init.rb
@@ -1,8 +1,10 @@
+require_dependency 'query_patch'
+
 Redmine::Plugin.register :parent_issue_filter do
-  name 'Parent Issue Filter plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Parent issue filter plugin'
+  author 'onozaty'
+  description 'Parent issue filter plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/onozaty/redmine-parent-issue-filter'
+  author_url 'https://github.com/onozaty'
 end
---
+++ lib/query_patch.rb
@@ -0,0 +1,31 @@
+module QueryPatch
+  def self.included(base) # :nodoc:
+    base.send(:include, InstanceMethods)
+
+    # Same as typing in the class
+    base.class_eval do
+      unloadable # Send unloadable so it will not be unloaded in development
+
+      alias_method_chain :available_filters, :parent_id
+    end
+  end
+  
+  module InstanceMethods
+
+    # Wrapper around the +available_filters+ to add a new Deliverable filter
+    def available_filters_with_parent_id
+      @available_filters = available_filters_without_parent_id
+      
+      parent_id_filters = {
+        "root_id" => { 
+          :name => l(:field_parent_issue),
+          :type => :integer, 
+          :order => @available_filters.size + 1}}
+
+      return @available_filters.merge(parent_id_filters)
+    end
+  end    
+end
+
+# Add module to Query
+Query.send(:include, QueryPatch)
