7cca6fa57602507153f6169f04774d2204d41b1d
root <root@localhost.localdomain>
2014-02-11 13:44:16 +0000

    initial commit.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :parent_issue_filter do
+  name 'Parent Issue Filter plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
