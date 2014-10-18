e2a306668038ad84df0c0d0b36bb8839d9a3d9d4
onozaty <onozaty@gmail.com>
2014-10-18 14:11:46 +0000

    Update version number

--- init.rb
+++ init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :parent_issue_filter do
   name 'Parent issue filter plugin'
   author 'onozaty'
   description 'Parent issue filter plugin for Redmine'
-  version '1.0.0'
+  version '1.0.1'
   url 'https://github.com/onozaty/redmine-parent-issue-filter'
   author_url 'https://github.com/onozaty'
 end
