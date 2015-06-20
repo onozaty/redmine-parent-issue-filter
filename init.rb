e806c3cbf0b67f574c6a86f603ad8750000c1110
onozaty <onozaty@gmail.com>
2015-06-20 13:56:51 +0000

    Fixed #7

--- init.rb
+++ init.rb
@@ -1,10 +1,10 @@
-require_dependency 'query_patch'
+require_dependency 'redmine_parent_issue_filter'
 
 Redmine::Plugin.register :parent_issue_filter do
   name 'Parent issue filter plugin'
   author 'onozaty'
   description 'Parent issue filter plugin for Redmine'
-  version '1.0.1'
+  version '1.0.2'
   url 'https://github.com/onozaty/redmine-parent-issue-filter'
   author_url 'https://github.com/onozaty'
 end
--- lib/redmine_parent_issue_filter.rb
+++ lib/redmine_parent_issue_filter.rb
@@ -1,36 +1,38 @@
-module QueryPatch
-  def self.included(base) # :nodoc:
-    base.send(:include, InstanceMethods)
+module RedmineParentIssueFilter
+  module QueryPatch
+    def self.included(base) # :nodoc:
+      base.send(:include, InstanceMethods)
 
-    # Same as typing in the class
-    base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
+      # Same as typing in the class
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
 
-      alias_method_chain :available_filters, :parent_id
+        alias_method_chain :available_filters, :parent_id
+      end
+    end
+  
+    module InstanceMethods
+
+      # Wrapper around the +available_filters+ to add a new Deliverable filter
+      def available_filters_with_parent_id
+        @available_filters = available_filters_without_parent_id
+      
+        parent_id_filters = {
+          "parent_id" => { 
+            :name => l(:field_parent_issue),
+            :type => :integer, 
+            :order => @available_filters.size + 1},
+          "root_id" => { 
+            :name => l(:field_root_issue),
+            :type => :integer, 
+            :order => @available_filters.size + 2}
+        }
+
+        return @available_filters.merge!(parent_id_filters)
+      end
     end
   end
-  
-  module InstanceMethods
-
-    # Wrapper around the +available_filters+ to add a new Deliverable filter
-    def available_filters_with_parent_id
-      @available_filters = available_filters_without_parent_id
-      
-      parent_id_filters = {
-        "parent_id" => { 
-          :name => l(:field_parent_issue),
-          :type => :integer, 
-          :order => @available_filters.size + 1},
-        "root_id" => { 
-          :name => l(:field_root_issue),
-          :type => :integer, 
-          :order => @available_filters.size + 2}
-      }
-
-      return @available_filters.merge!(parent_id_filters)
-    end
-  end    
 end
 
 # Add module to Query
-Query.send(:include, QueryPatch)
+Query.send(:include, RedmineParentIssueFilter::QueryPatch)
